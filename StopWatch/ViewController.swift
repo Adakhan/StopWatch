f12b1e60840843600acf3e2c2fa21f3a0c6ef088
adakhan <adakhanau@gmail.com>
2019-04-09 13:46:55 +0600

    Add StopWatch App

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Adakhanau on 03/04/2019.
+//  Copyright © 2019 Adakhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    //Labels
+    
+    @IBOutlet weak var timeScreen: UILabel!
+    @IBOutlet weak var startLabel: UIButton!
+    @IBOutlet weak var pauseLabel: UIButton!
+    @IBOutlet weak var resetLabel: UIButton!
+    
+    
+    //Proporties
+    
+    var time = 0
+    var timer = Timer()
+    var isTimerRunning = false
+    var resumeTapped = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pauseLabel.isEnabled = false
+        resetLabel.isEnabled = false
+    }
+    
+    
+    //Actions
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        if isTimerRunning == false {
+            runTimer()
+            pauseLabel.isEnabled = true
+            resetLabel.isEnabled = true
+            startLabel.isEnabled = false
+
+        }
+        
+    }
+    
+    @IBAction func pauseButton(_ sender: UIButton) {
+        if resumeTapped == false {
+            timer.invalidate()
+            resumeTapped = true
+            pauseLabel.setTitle("Resume", for: .normal)
+        } else {
+            runTimer()
+            resumeTapped = false
+            pauseLabel.setTitle("Pause", for: .normal)
+        }
+        
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        timer.invalidate()
+        isTimerRunning = false
+        resumeTapped = false
+        pauseLabel.isEnabled = false
+        startLabel.isEnabled = true
+        resetLabel.isEnabled = false
+        time = 0
+        timeScreen.text = String(time)
+        pauseLabel.setTitle("Pause",for: .normal)
+        
+    }
+    
+    
+    //functions
+    
+    func runTimer() {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+        
+        isTimerRunning = true
+        
+    }
+    
+    
+    @objc func action() {
+        
+        time += 1
+        timeScreen.text = String(time)
+    }
+    
+}
+
